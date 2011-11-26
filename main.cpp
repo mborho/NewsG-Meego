8f59976591eea93781b8ef94dafa1df95a62d370
mborho <martin@borho.net>
2011-11-26 15:00:56 +0100

    some main.cpp tweaks

--- main.cpp
+++ main.cpp
@@ -8,8 +8,34 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     viewer->setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer->setMainQmlFile(QLatin1String("qml/newsg/main.qml"));
+
+    viewer->setAttribute(Qt::WA_OpaquePaintEvent);
     viewer->setAttribute(Qt::WA_NoSystemBackground);
+    viewer->viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+    viewer->viewport()->setAttribute(Qt::WA_NoSystemBackground);
+    viewer->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+
     viewer->showExpanded();
 
     return app->exec();
 }
+
+//#include <QApplication>
+//#include <QDeclarativeView>
+
+//int main(int argc, char **argv)
+//{
+//    QApplication a(argc, argv);
+//    QDeclarativeView view;
+//    view.setSource(QUrl("/opt/newsg/qml/newsg/main.qml"));
+//    view.setAttribute(Qt::WA_OpaquePaintEvent);
+
+//    view.setAttribute(Qt::WA_NoSystemBackground);
+//    view.viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+//    view.viewport()->setAttribute(Qt::WA_NoSystemBackground);
+//    view.setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+
+//    view.showFullScreen();
+//    return a.exec();
+//}
+
